550337ede82e1d9c2be870376bc93f5d7fdd0dbe
Sean Nam <namsangwoo1@gmail.com>
2017-10-27 02:00:42 -0700

    Created ScheduleViewModel, added table to ScheduleView, added some test data

--- ucsdscheduleme/Controllers/ScheduleController.cs
+++ ucsdscheduleme/Controllers/ScheduleController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
@@ -19,41 +20,81 @@ namespace ucsdscheduleme.Controllers
         public IActionResult GenerateSchedule(string course1, string course2, 
                                               string course3, string course4) {
 
-            Console.WriteLine("hello world");
-            Console.WriteLine("course1:" + course1);
-            Console.WriteLine("course2:" + course2);
-            Console.WriteLine("course3:" + course3);
-            Console.WriteLine("course4:" + course4);
-            //var schedule = new List<Course>();
 
-            //var c1 = new Course { CourseName = course1 };
-            //var c2 = new Course { CourseName = course2 };
-            //var c3 = new Course { CourseName = course3 };
-            //var c4 = new Course { CourseName = course4 };
+            // Used to return a list courses. 
+            var schedule = new ScheduleViewModel();
 
-            //Console.WriteLine("c1:" + c1.CourseName);
-            //Console.WriteLine("c2:" + c2.CourseName);
+            // TODO Retrieve course object from database based on course name.
+            // For now, just sort and create new Course object for each string 
+            // passed in.
+            var list = new List<string>
+            {
+                course1, course2, course3, course4
+            };
+            list.Sort();
 
-            //schedule.Add(c1);
-            //schedule.Add(c2);
-            //schedule.Add(c3);
-            //schedule.Add(c4);
-
-            var resultList = new List<string>{
-                course1,
-                course2,
-                course3,
-                course4
+            var c1 = new Course 
+            { 
+                Id = 1,
+                CourseName = list[0],
+                Units = 4,
+                Sections = new List<Section> {
+                    new Section
+                    {
+                        Id = 10,
+                        CourseId = 1,
+                        ProfessorId = 100,
+                        Meetings = new List<Meeting>
+                        {
+                            new Meeting
+                            {
+                                Id = 101,
+                                SectionId = 10,
+                                StartTime = 10,
+                                EndTime = 11
+                            }
+                        }
+                    }
+                }
             };
 
-            ViewData["course1"] = course1;
-            ViewData["course2"] = course2;
-            ViewData["course3"] = course3;
-            ViewData["course4"] = course4;
+            var c2 = new Course
+            {
+                Id = 2,
+                CourseName = list[1],
+                Units = 4,
+                Sections = new List<Section> {
+                    new Section
+                    {
+                        Id = 10,
+                        CourseId = 2,
+                        ProfessorId = 100,
+                        Meetings = new List<Meeting>
+                        {
+                            new Meeting
+                            {
+                                Id = 102,
+                                SectionId = 10,
+                                StartTime = 9,
+                                EndTime = 11
+                            }
+                        }
+                    }
+                }
+            };
 
-            ViewData["courseList"] = resultList;
+            var c3 = new Course { CourseName = list[2] };
+            var c4 = new Course { CourseName = list[3] };
 
-            return View("Index");
+            // TODO Call schedule algorithm on four classes.
+
+
+            schedule.CourseList.Add(c1);
+            schedule.CourseList.Add(c2);
+            schedule.CourseList.Add(c3);
+            schedule.CourseList.Add(c4);
+
+            return View(nameof(Index), schedule);
         }
     }
 }
---
+++ ucsdscheduleme/Models/ScheduleViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ucsdscheduleme.Models
+{
+    public class ScheduleViewModel
+    {
+        public List<Course> CourseList { get; set; } = new List<Course>();
+    }
+}
