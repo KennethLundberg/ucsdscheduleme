a55f188bdbefdf129c144b2036e7082bcfee6247
Sean Nam <namsangwoo1@gmail.com>
2017-10-29 15:19:20 -0700

    Query DB, call find schedule algorithm, and return Schedule

--- ucsdscheduleme/Controllers/ScheduleController.cs
+++ ucsdscheduleme/Controllers/ScheduleController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using ucsdscheduleme.Models;
 using ucsdscheduleme.Data;
+using System.Linq;
+using ucsdscheduleme.Repo;
 
 namespace ucsdscheduleme.Controllers
 {
@@ -24,110 +26,32 @@ namespace ucsdscheduleme.Controllers
         /// Form postback that generates the schedule based on four strings passed.
         /// </summary>
         /// <returns>The four courses in sorted order</returns>
-        /// <param name="course1">Course1.</param>
-        /// <param name="course2">Course2.</param>
-        /// <param name="course3">Course3.</param>
-        /// <param name="course4">Course4.</param>
-        public IActionResult GenerateSchedule(string course1, string course2, 
-                                              string course3, string course4) {
+        /// <param name="course1name">name of Course 1.</param>
+        /// <param name="course2name">name of Course 2.</param>
+        /// <param name="course3name">name of Course 3.</param>
+        /// <param name="course4name">name of Course 4.</param>
+        public IActionResult GenerateSchedule(string course1name, string course2name, 
+                                              string course3name, string course4name) {
 
-
+            var course1 = _context.Courses
+                                  .Single(c => c.CourseAbbreviation.Contains(course1name));
+            var course2 = _context.Courses
+                                  .Single(c => c.CourseAbbreviation.Contains(course2name));
+            var course3 = _context.Courses
+                                  .Single(c => c.CourseAbbreviation.Contains(course3name));
+            var course4 = _context.Courses
+                                  .Single(c => c.CourseAbbreviation.Contains(course4name));
+            
             // Used to return a list courses. 
             var schedule = new ScheduleViewModel();
 
-            // TODO Retrieve course object from database based on course name.
-            // For now, just sort and create new Course object for each string 
-            // passed in.
-            var list = new List<string>
-            {
-                course1, course2, course3, course4
-            };
-            list.Sort();
+            Course[] courses = new Course[] { course1, course2, course3, course4 };
 
-            var c1 = new Course
-            {
-                Id = 1,
-                CourseName = list[0],
-                Units = 4,
-                Sections = new List<Section> {
-                    new Section
-                    {
-                        Id = 10,
-                        Meetings = new List<Meeting>
-                        {
-                            new Meeting
-                            {
-                                Id = 101,
-                                MeetingType = MeetingType.Lecture,
-                                StartTime = 10,
-                                EndTime = 11
-                            },
-                            new Meeting
-                            {
-                                Id = 102,
-                                MeetingType = MeetingType.Discussion,
-                                StartTime = 20,
-                                EndTime = 21
-                            }
-                        }
-                    },
-                    new Section
-                    {
-                        Id = 10,
-                        Meetings = new List<Meeting>
-                        {
-                            new Meeting
-                            {
-                                Id = 101,
-                                MeetingType = MeetingType.Lecture,
-                                StartTime = 10,
-                                EndTime = 11
-                            },
-                            new Meeting
-                            {
-                                Id = 102,
-                                MeetingType = MeetingType.Discussion,
-                                StartTime = 20,
-                                EndTime = 21
-                            }
-                        }
-                    }
-                }
-            };
+            var scheduleRepo = new ScheduleRepo();
 
-            var c2 = new Course
-            {
-                Id = 2,
-                CourseName = list[1],
-                Units = 4,
-                Sections = new List<Section> {
-                    new Section
-                    {
-                        Id = 10,
-                        Meetings = new List<Meeting>
-                        {
-                            new Meeting
-                            {
-                                Id = 102,
-                                MeetingType = MeetingType.Discussion,
-                                StartTime = 9,
-                                EndTime = 11
-                            }
-                        }
-                    }
-                }
-            };
-
-            var c3 = new Course { CourseName = list[2] };
-            var c4 = new Course { CourseName = list[3] };
-
-            // TODO Call schedule algorithm on four classes.
-
-
-            schedule.CourseList.Add(c1);
-            schedule.CourseList.Add(c2);
-            schedule.CourseList.Add(c3);
-            schedule.CourseList.Add(c4);
+            // Call the schedule finding algorithm.
+            var data = scheduleRepo.FindScheduleForClasses(courses);
+            schedule = data[0];
 
             return View(nameof(Index), schedule);
         }
--- ucsdscheduleme/Models/ScheduleViewModel.cs
+++ ucsdscheduleme/Models/ScheduleViewModel.cs
@@ -6,5 +6,10 @@ namespace ucsdscheduleme.Models
     public class ScheduleViewModel
     {
         public List<Course> CourseList { get; set; } = new List<Course>();
+
+        public static implicit operator ScheduleViewModel(List<Section> v)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
