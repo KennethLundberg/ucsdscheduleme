1ffae737285b4666f8510ab67874f6031ff98435
jltien <jltien@ucsd.edu>
2017-11-02 20:05:34 -0700

    Debugging
    Created tree and nodes are the classes. All nodes are connected to the nodes in the layer under it. Graph is correct, but DFS is not tranversing the tree correctly. Pushing for Sean to debug on his computer.

---
+++ ucsdscheduleme/Controllers/TestScheduleController.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using ucsdscheduleme.Models;
+using ucsdscheduleme.Repo;
+namespace ucsdscheduleme.Controllers
+{
+    public class TestScheduleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }    
+        public IActionResult GenerateSchedule(string course1, string course2, string course3, string course4) {
+            {
+                Meeting CSE20Lecture = new Meeting()
+                {
+                    MeetingType = MeetingType.Lecture,
+                    Days = (Days.Tuesday | Days.Thursday),
+                    StartTime = 1400,
+                    EndTime = 1520,
+                    Code = "A00"
+                };
+                Meeting CSE20Discussion1 = new Meeting()
+                {
+                    MeetingType = MeetingType.Discussion,
+                    Days = Days.Wednesday,
+                    StartTime = 1000,
+                    EndTime = 1050,
+                    Code = "A02"
+                };
+                Meeting CSE20Discussion2 = new Meeting()
+                {
+                    MeetingType = MeetingType.Discussion,
+                    Days = (Days.Tuesday | Days.Thursday),
+                    StartTime = 1711,
+                    EndTime = 1712,
+                    Code = "B02"
+                };
+                Course CSE20 = new Course()
+                {
+                    CourseAbbreviation = "CSE20",
+                    CourseName = "Intro / Discrete Mathematics",
+                    Units = 4,
+                    Description = ""
+                };
+                Section CSE20Section1 = new Section()
+                {
+                    Ticket = 915105,
+                    Course = CSE20
+                };
+                Section CSE20Section2 = new Section()
+                {
+                    Ticket = 915105,
+                    Course = CSE20
+                };
+                // Adding different sections to the course object
+                CSE20.Sections.Add(CSE20Section1);
+                CSE20Section1.Meetings.Add(CSE20Lecture);
+                CSE20Section1.Meetings.Add(CSE20Discussion1);
+                CSE20Section2.Meetings.Add(CSE20Lecture);
+                CSE20Section2.Meetings.Add(CSE20Discussion2);
+                CSE20.Sections.Add(CSE20Section2);
+
+                Meeting CSE21Lecture = new Meeting()
+                {
+                    MeetingType = MeetingType.Lecture,
+                    Days = (Days.Tuesday | Days.Thursday),
+                    StartTime = 1700,
+                    EndTime = 1710,
+                    Code = "A00"
+                };
+                Meeting CSE21Discussion1 = new Meeting()
+                {
+                    MeetingType = MeetingType.Discussion,
+                    Days = Days.Wednesday,
+                    StartTime = 900,
+                    EndTime = 901,
+                    Code = "A02"
+                };
+                Meeting CSE21Discussion2 = new Meeting()
+                {
+                    MeetingType = MeetingType.Discussion,
+                    Days = Days.Wednesday,
+                    StartTime = 903,
+                    EndTime = 904,
+                    Code = "A02"
+                }; 
+                Course CSE21 = new Course()
+                {
+                    CourseAbbreviation = "CSE21",
+                    CourseName = "Intro / Discrete Mathematics",
+                    Units = 4,
+                    Description = ""
+                };
+
+                Section CSE21Section1 = new Section()
+                {
+                    Ticket = 915105,
+                    Course = CSE21
+                };
+                Section CSE21Section2 = new Section()
+                {
+                    Ticket = 915105,
+                    Course = CSE21
+                }; 
+                // Adding different sections to the course object
+                CSE21.Sections.Add(CSE21Section1);
+                CSE21.Sections.Add(CSE21Section2);
+                CSE21Section1.Meetings.Add(CSE21Lecture);
+                CSE21Section1.Meetings.Add(CSE21Discussion1);
+                CSE21Section2.Meetings.Add(CSE21Lecture);
+                CSE21Section2.Meetings.Add(CSE21Discussion2);
+
+                Meeting CSE101Lecture = new Meeting()
+                {
+                    MeetingType = MeetingType.Lecture,
+                    Days = (Days.Tuesday | Days.Thursday),
+                    StartTime = 1,
+                    EndTime = 2,
+                    Code = "A00"
+                };
+                Meeting CSE101Discussion1 = new Meeting()
+                {
+                    MeetingType = MeetingType.Discussion,
+                    Days = Days.Wednesday,
+                    StartTime = 3,
+                    EndTime = 4,
+                    Code = "A02"
+                };
+                Meeting CSE101Discussion2 = new Meeting()
+                {
+                    MeetingType = MeetingType.Discussion,
+                    Days = (Days.Tuesday | Days.Thursday),
+                    StartTime = 5,
+                    EndTime = 6,
+                    Code = "B02"
+                };
+                Course CSE101 = new Course()
+                {
+                    CourseAbbreviation = "CSE101",
+                    CourseName = "ALGORITHMS",
+                    Units = 4,
+                    Description = ""
+                };
+                Section CSE101Section1 = new Section()
+                {
+                    Ticket = 915104,
+                    Course = CSE101
+                };
+                Section CSE101Section2 = new Section()
+                {
+                    Ticket = 915103,
+                    Course = CSE101
+                };
+                // Adding different sections to the course object
+                CSE101.Sections.Add(CSE101Section1);
+                CSE101Section1.Meetings.Add(CSE101Lecture);
+                CSE101Section1.Meetings.Add(CSE101Discussion1);
+                CSE101Section2.Meetings.Add(CSE101Lecture);
+                CSE101Section2.Meetings.Add(CSE101Discussion2);
+                CSE101.Sections.Add(CSE101Section2);
+
+                ScheduleRepo repo = new ScheduleRepo();
+                Course[] courses = { CSE20, CSE21, CSE101 };
+                List<List<Section>> returned;
+                returned = repo.FindScheduleForClasses(courses);
+                Console.WriteLine("RETURNED SCHEDULE NUM: " + returned.Count);
+                foreach(List<Section> l in returned) {
+                    foreach(Section s in l) 
+                    {
+                        Console.Write(s.Course.CourseAbbreviation + " ");    
+                    }
+                    Console.WriteLine(" ");
+                }
+                return View();
+            }
+        }
+    }
+}
--- ucsdscheduleme/Repo/ScheduleRepo.cs
+++ ucsdscheduleme/Repo/ScheduleRepo.cs
@@ -18,8 +18,8 @@ namespace ucsdscheduleme.Repo
         public Section Section { get; set; }
 
         public readonly int Index;
-        public bool IsFirstClass { get; set; }
-
+        public int numEdges { get; set; }
+        public int currEdges { get; set; }
         public List<Node> Edges { get; set; } = new List<Node>();
     }
 
@@ -32,59 +32,128 @@ namespace ucsdscheduleme.Repo
         /// <returns>List of possible schedules for course list.</returns>
         public List<List<Section>> FindScheduleForClasses(Course[] coursesToSchedule)
         {
-            
-            List<Node> AllSections = new List<Node>();
+
+            //         List<Node> AllSections = new List<Node>();
             List<List<Section>> possibleSchedules = new List<List<Section>>();
 
-            if (coursesToSchedule.Count() == 1) 
-            { 
+            if (coursesToSchedule.Count() == 1)
+            {
                 possibleSchedules.Add((System.Collections.Generic.List<ucsdscheduleme.Models.Section>)coursesToSchedule[0].Sections);
-                return possibleSchedules;    
+                return possibleSchedules;
             }
 
             Course firstCourse = coursesToSchedule[0];
             int numClasses = coursesToSchedule.Length;
 
             // Create a node for each meeting.
+            /*            int numNodes = 0;
+                        foreach (Course course in coursesToSchedule)
+                        {
+                            foreach (Section section in course.Sections)
+                            {
+                                Node thisSection = new Node(numNodes++)
+                                {
+                                    Course = course,
+                                    Section = section
+                                };
+                                AllSections.Add(thisSection);
+                                thisSection.IsFirstClass = thisSection.Course == firstCourse;
+                            }
+                        } */
+
+            // Create a node for each meeting.
             int numNodes = 0;
-            foreach (Course course in coursesToSchedule)
+            List<List<Node>> AllSections = new List<List<Node>>();
+
+            foreach (Course i in coursesToSchedule) 
             {
-                foreach (Section section in course.Sections)
+                List<Node> thisCourse = new List<Node>();
+                //) (int i = 0; i < coursesToSchedule.Count(); i++)   
+                foreach (Section j in i.Sections)
                 {
                     Node thisSection = new Node(numNodes++)
                     {
-                        Course = course,
-                        Section = section
+                        Course = i,
+                        Section = j,
+                        numEdges = 0,
+                        currEdges = 0
                     };
-                    AllSections.Add(thisSection);
-                    thisSection.IsFirstClass = thisSection.Course == firstCourse;
+                    Console.WriteLine(thisSection.Course.CourseAbbreviation);
+                    thisCourse.Add(thisSection);
                 }
+                AllSections.Add(thisCourse);
             }
 
+
+  //          Console.WriteLine("number of nodes:" + numNodes);
             // Create all the edges between meetings
-            for (int i = 0; i < AllSections.Count(); i++)
-            {
-                for (int j = i + 1; j < AllSections.Count(); j++)
-                {
-                    Node node1 = AllSections[i];
-                    Node node2 = AllSections[j];
-                    if (node1.Course != node2.Course)
-                    {
-                        if (!Conflict(node1.Section, node2.Section))
+            /*            for (int i = 0; i < AllSections.Count(); i++)
                         {
-                            node1.Edges.Add(node2);
+                            for (int j = i + 1; j < AllSections.Count(); j++)
+                            {
+                                Node node1 = AllSections[i];
+                                Node node2 = AllSections[j];
+                                if (node1.Course != node2.Course)
+                                {
+                                    if (!Conflict(node1.Section, node2.Section))
+                                    {
+                                        node1.Edges.Add(node2);
+                                    }
+                                }
+                            }
                         }
+                        */
+            // Create all the edges between meetings
+            for (int i = 0; i < coursesToSchedule.Count() - 1; i++)
+            {
+//                Console.WriteLine("i" + i);
+  //              Console.WriteLine("Num of Courses: " + AllSections.Count());
+                foreach (Node j in AllSections[i])
+                {
+//                    Console.WriteLine("j: " + j.Course.CourseAbbreviation);
+                    foreach (Node k in AllSections[i + 1])
+                    {
+                        j.Edges.Add(k);
+                        j.numEdges++;
+                        j.currEdges++;
                     }
                 }
             }
-
-            // dfs on graph to find possible schedules
-            foreach (Node section in AllSections)
+/*            Console.WriteLine("GRAPH CHECK: ");
+            foreach (Node i in AllSections[0]) {
+                Console.WriteLine("COURSE: " + i.Course.CourseAbbreviation);   
+                Console.WriteLine("NUM EDGES: " + i.Edges.Count()); 
+            }
+            foreach (Node i in AllSections[1])
             {
-                if (section.IsFirstClass)
+                Console.WriteLine("COURSE: " + i.Course.CourseAbbreviation);
+                Console.WriteLine("NUM EDGES: " + i.Edges.Count());
+                Console.WriteLine(i.Edges.ElementAt(0).Course.CourseAbbreviation);
+            } */
+            // dfs on graph to find possible schedules
+            /*           foreach (Node section in AllSections)
+                       {
+                           if (section.IsFirstClass)
+                           {
+                               DFS(section, numClasses, numNodes, ref possibleSchedules);
+                               Console.WriteLine("SIZE OF POSS:" + possibleSchedules.Count());
+                           }
+                       }
+                       return possibleSchedules;
+                   } */
+            foreach (Node i in AllSections[0])
+            { 
+                List<Section> schedule = new List<Section>();
+            DFS(i, numClasses, ref schedule, ref possibleSchedules);
+                Console.WriteLine("NUM OF SCH AFTER DFS CALL: " + possibleSchedules.Count());
+                for (int j = 0; j < AllSections.Count; j++)
                 {
-                    DFS(section, numClasses, numNodes, ref possibleSchedules);
-                }
+                    List<Node> course = AllSections[j];
+                    for (int k = 0; k < course.Count(); k++) 
+                    {
+                        AllSections[j][k].currEdges = AllSections[j][k].numEdges;
+                    }
+                } 
             }
             return possibleSchedules;
         }
@@ -95,35 +164,83 @@ namespace ucsdscheduleme.Repo
         /// <param name="root">Node to do DFS from</param>
         /// <param name="numClasses">Total number of classes to schedule</param>
         /// <param name="possibleSchedules">Modified list of possible schedules</param>
-        private void DFS(Node root, int numClasses, int numNodes, ref List<List<Section>> possibleSchedules)
+        private void DFS(Node section, int numClasses, ref List<Section> currSchedule, ref List<List<Section>> possibleSchedules)
         {
+            Console.WriteLine("DFSCALL: " + section.Course.CourseAbbreviation);
+            currSchedule.Add(section.Section);
+            foreach (Section z in currSchedule) {
+                Console.WriteLine(z.Course.CourseAbbreviation);
+            }
+            if (section.Edges.Count() == 0) 
+            {
+                if (currSchedule.Count() == numClasses) {
+                    //  if (!Conflict(currSchedule))
+                     {
+                        List<Section> validSchedule = new List<Section>();
+                        foreach(Section k in currSchedule) {
+                            validSchedule.Add(k);
+                        }
+                        possibleSchedules.Add(validSchedule);
+                    }
+                }
+                currSchedule.Remove(currSchedule.Last());
+ 
+                return;
+            }
+            //if (section.numEdges == 0) 
+            //{
+            //    Console.WriteLine("last = " + currSchedule.Last());
+            //    currSchedule.Remove(currSchedule.Last());
+            //}
+            foreach (Node i in section.Edges) 
+            {
+                DFS(i, numClasses, ref currSchedule, ref possibleSchedules);
+                i.currEdges--;
+                if (i.currEdges == 0)
+                {
+                    Console.WriteLine("last = " + currSchedule.Last().Course.CourseAbbreviation);
+                    currSchedule.Remove(currSchedule.Last());
+   //                 i.currEdges = i.numEdges;
+                }
+                //section.Edges.Remove(i);
+            }
+        }
+        /// <summary>
+        /// Performs DFS on schedule graph.
+        /// </summary>
+        /// <param name="root">Node to do DFS from</param>
+        /// <param name="numClasses">Total number of classes to schedule</param>
+        /// <param name="possibleSchedules">Modified list of possible schedules</param>
+/*        private void DFS(Node root, int numClasses, int numNodes, ref List<List<Section>> possibleSchedules)
+        {
+//            Console.WriteLine("DFS CALL");
             Node curr;
             Stack<Node> stack = new Stack<Node>();
-            bool[] isNodeVisited = new bool[numNodes];
+  //          bool[] isNodeVisited = new bool[numNodes];
             List<Section> temp = new List<Section>
             {
                 root.Section
             };
 
             stack.Push(root);
-            isNodeVisited[0] = true;
+//            isNodeVisited[0] = true;
 
             while (stack.Count != 0)
             {
                 curr = stack.Peek();
                 stack.Pop();
-                if (curr != root)
-                {
-                    temp.Remove(curr.Section);
-                }
+ 
+//                    temp.Remove(temp.Last());
+ //               }
                 foreach (Node i in curr.Edges)
                 {
-                    if (!isNodeVisited[i.Index] && !Conflict(i.Section, temp))
-                    {
+                    Console.WriteLine("NUM CLASSES IN TEMP RN: " + temp.Count());
+/*                    if (/*!isNodeVisited[i.Index] && !Conflict(i.Section, temp)) */
+/*                    {
                         stack.Push(i);
                         temp.Add(i.Section);
-                        isNodeVisited[i.Index] = true;
-
+                        //                        isNodeVisited[i.Index] = true;
+                        if (temp.Last(). != root)
                         if (temp.Count() == numClasses)
                         {
                             List<Section> possibleSchedule = new List<Section>();
@@ -132,11 +249,13 @@ namespace ucsdscheduleme.Repo
                                 possibleSchedule.Add(j);
                             }
                             possibleSchedules.Add(possibleSchedule);
+                            Console.Write("NUM OF POSS: " + possibleSchedule.Count());
+                            temp.Remove(temp.Last());
                         }
                     }
                 }
             }
-        }
+        } */
 
         /// <summary>
         /// Checks for time conflicts between two nodes.
@@ -166,13 +285,12 @@ namespace ucsdscheduleme.Repo
         /// Checks for time conflicts between a node and the existing schedule.
         /// </summary>
         /// <returns>True if there is a conflict, else false.</returns>
-        /// <param name="section1">First node to check conflicts for</param>
-        /// <param name="temp">Current existing schedule</param>
-        private bool Conflict(Section section1, List<Section> temp)
+        /// <param name="currSchedule">First node to check conflicts for</param>
+        private bool Conflict(List<Section> currSchedule)
         {
-            foreach (Section i in temp)
+            for (int i = 0; i < currSchedule.Count() - 1; i++) 
             {
-                if (Conflict(section1, i))
+                if (Conflict(currSchedule.ElementAt(i), currSchedule.ElementAt(i+1)))
                 {
                     return true;
                 }
