93fae293bb3f2b76495f0da454ba8c8519af92ce
Ryan Nguyen <xryanguyenx@gmail.com>
2017-11-10 14:41:03 -0800

    Modified generate schedule function
    created the optimization eums
    created the generate schedule controller action

--- ucsdscheduleme/Controllers/HomeController.cs
+++ ucsdscheduleme/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ucsdscheduleme.Models;
+using ucsdscheduleme.Repo;
 
 namespace ucsdscheduleme.Controllers
 {
@@ -32,6 +33,48 @@ namespace ucsdscheduleme.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult GenerateSchedule(Course[] courses, Optimization optimization)
+        {
+            // Used to return a list courses. 
+       
+            var scheduleRepo = new ScheduleRepo();
+
+            // Call the schedule finding algorithm.
+            var data = scheduleRepo.FindScheduleForClasses(courses);
+
+            List<Section> schedule = data[0];
+
+            switch (optimization)
+            {
+                case Optimization.HighestGPA:
+                    //scheduule = functioin(data);
+                    break;
+                case Optimization.HighestRMP:
+                    //scheduule = functioin(data);
+                    break;
+                case Optimization.EarlyEnd:
+                    //scheduule = functioin(data);
+                    break;
+                case Optimization.LateStart:
+                    //scheduule = functioin(data);
+                    break;
+                case Optimization.MostDays:
+                    //scheduule = functioin(data);
+                    break;
+                case Optimization.LeastDays:
+                    //scheduule = functioin(data);
+                    break;
+                case Optimization.MostGaps:
+                    //scheduule = functioin(data);
+                    break;
+                case Optimization.LeastGaps:
+                    //scheduule = functioin(data);
+                    break;
+            }
+
+            return Json(schedule);
         }
     }
 }
