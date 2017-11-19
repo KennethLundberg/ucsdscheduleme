e85255da3ee9a5f232ec814693876d0a4ba39fca
jltien <jltien@ucsd.edu>
2017-11-18 17:00:31 -0800

    Clears the input
    Clears but the input but the call to the server is throwing an error

--- ucsdscheduleme/wwwroot/js/site.js
+++ ucsdscheduleme/wwwroot/js/site.js
@@ -8,3 +8,39 @@ function insertMetadata(metadata)
 {
 
 }
+
+function typeAhead(e) {
+    console.log("working!");
+    clearSearch();
+    var input = e.target.value;
+    var xhr = new XMLHttpRequest();
+    console.log(myApp.urls.typeAhead);
+    xhr.open("POST", myApp.urls.typeAhead, true);    
+    xhr.send(input);
+
+/*    xhr.onreadystatechange = function() 
+    {
+        if (xhr.readyState == 4 && xhr.status == 200)
+        {
+            clearSearch();
+            for (i = 0; i < xhr.responseText.length(); i++)
+            {
+                populateSearch(xhr.responseText[i]);
+            }
+        }
+    }*/ 
+}
+
+function clearSearch()
+{
+    var courses = document.getElementsByClassName("courseItem");
+    while (courses[0])
+    {
+        courses[0].remove();
+    }
+}
+
+function populateSearch(data)
+{
+
+}
