509c63da40efc3e74544a678ab5718b4dda73c31
Ryan Nguyen <xryanguyenx@gmail.com>
2017-12-09 17:54:37 -0800

    Fixed the alert messages to send out through the controller instead of the view.

--- ucsdscheduleme/wwwroot/js/site.js
+++ ucsdscheduleme/wwwroot/js/site.js
@@ -973,18 +973,10 @@ function generateSchedule() {
         if (xhr.readyState == 4 && xhr.status == 200) {
             var response = JSON.parse(xhr.responseText);
             if (response.error != "") {
-                if (response.courses == null) {
-                    myApp.errors.push("Please add a class to generate a schedule.");
-                    showAlert();
-                } else if (response.error == null) {
-                    myApp.errors.push("An unknown error occurred.");
-                    showAlert();
-                    return;
-                } else {
-                    myApp.errors.push(response.error);
-                    showAlert();
-                    return;
-                }
+                console.log(response.error);
+                myApp.errors.push(response.error);
+                showAlert();
+                return;
             }
 
             updateSchedule(response.courses);
